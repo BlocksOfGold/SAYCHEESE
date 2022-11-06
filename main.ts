cad5b8a561bf42988cec48d383c083d403dddb02
BlocksOfGold <117589512+BlocksOfGold@users.noreply.github.com>
2022-11-06 11:10:45 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showIcon(IconNames.Fabulous)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Silly)
+    basic.showIcon(IconNames.Surprised)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
